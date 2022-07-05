77c3a4a4209e0d9cad754641d6ee5fe8775244e5
Abuabduillah <imransuleiman26@gmail.com>
2022-07-05 14:59:52 -0700

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - a function that prints 10 times the alphabet
- * 
+ *
  * Return: x10 a-z
  */
 void print_alphabet_x10(void)
@@ -19,4 +19,4 @@ void print_alphabet_x10(void)
 		co++;
 		_putchar('\n');
 	}
-}	
+}
