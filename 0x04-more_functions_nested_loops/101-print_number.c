1081a7bc222a38a11631d50a45696491a7e11b30
Abuabduillah <imransuleiman26@gmail.com>
2022-07-07 17:55:01 -0700

    esfsce

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,18 +5,19 @@
  * @n: input integer parameter
  * Return: void
  */
-void print_number(int n)
-{
-	unsigned int i = n;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}	
-	if (i / 10)
-	{
-		print_number(i / 10);print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
-}	
+void print_number(int n)
+
+{
+unsigned int x;
+x = n;
+if (n < 0)
+{
+_putchar(45);
+x = -n;
+}
+if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
+
+}
